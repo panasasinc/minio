2d08e8ea7d927b2f5e3efe3e7c937bad67bb1bd8
Lech Lorens <lech_lorens@epam.com>
2023-04-06 10:42:22 +0200

    Implement bucket metadata cache

---
+++ cmd/bucket_metadata_cache.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"errors"
+	"sync"
+)
+
+var errCacheEntryNotFound = errors.New("Entry not found in cache")
+
+type BucketMetadataCache struct {
+	lock     sync.Mutex
+	metadata *map[string]BucketMetadata
+}
+
+func NewBucketMetadataCache() *BucketMetadataCache {
+	return &BucketMetadataCache{
+		metadata: new(map[string]BucketMetadata),
+	}
+}
+
+// Get fetches the given value from the cache
+func (cache *BucketMetadataCache) Get(bucket string) (BucketMetadata, error) {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+
+	meta, ok := (*cache.metadata)[bucket]
+	if ok {
+		return meta, nil
+	}
+
+	return BucketMetadata{}, errCacheEntryNotFound
+}
+
+// Set sets the value for the given entry in the cache
+func (cache *BucketMetadataCache) Set(bucket string, metadata BucketMetadata) {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+}
+
+// Delete removes a given entry from the cache
+func (cache *BucketMetadataCache) Delete(bucket string) {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+}
+
+// Drop removes all the entries from the cache
+func (cache *BucketMetadataCache) Drop() {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+	cache.metadata = new(map[string]BucketMetadata)
+}
