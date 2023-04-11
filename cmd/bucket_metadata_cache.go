e150ac35f56c1a91a3f7ea28ebdc87957207723c
Lech Lorens <lech_lorens@epam.com>
2023-04-11 15:42:15 +0200

    Fix bucket metadata cache creation

--- cmd/bucket_metadata_cache.go
+++ cmd/bucket_metadata_cache.go
@@ -9,13 +9,13 @@ var errCacheEntryNotFound = errors.New("Entry not found in cache")
 
 type BucketMetadataCache struct {
 	lock     sync.Mutex
-	metadata *map[string]BucketMetadata
+	metadata map[string]BucketMetadata
 }
 
 // NewBucketMetadataCache returns a pointer to a newly created BucketMetadataCache object
 func NewBucketMetadataCache() *BucketMetadataCache {
 	return &BucketMetadataCache{
-		metadata: new(map[string]BucketMetadata),
+		metadata: make(map[string]BucketMetadata),
 	}
 }
 
@@ -27,7 +27,7 @@ func (cache *BucketMetadataCache) Get(bucket string) (BucketMetadata, error) {
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
 
-	meta, ok := (*cache.metadata)[bucket]
+	meta, ok := cache.metadata[bucket]
 	if ok {
 		return meta, nil
 	}
@@ -42,7 +42,7 @@ func (cache *BucketMetadataCache) Set(bucket string, metadata BucketMetadata) {
 	}
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
-	(*cache.metadata)[bucket] = metadata
+	cache.metadata[bucket] = metadata
 }
 
 // Delete removes a given entry from the cache
@@ -52,7 +52,7 @@ func (cache *BucketMetadataCache) Delete(bucket string) {
 	}
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
-	delete(*cache.metadata, bucket)
+	delete(cache.metadata, bucket)
 }
 
 // Drop removes all the entries from the cache
@@ -62,5 +62,5 @@ func (cache *BucketMetadataCache) Drop() {
 	}
 	cache.lock.Lock()
 	defer cache.lock.Unlock()
-	cache.metadata = new(map[string]BucketMetadata)
+	cache.metadata = make(map[string]BucketMetadata)
 }
